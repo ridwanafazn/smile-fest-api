e25b71b7e2ed8d0a5d62c97f2daebb5d6d590fc4
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2026-05-13 19:43:59 +0700

    add - new fe pages for grant access by cors

--- internal/router/router.go
+++ internal/router/router.go
@@ -18,7 +18,7 @@ func SetupRouter() *gin.Engine {
 
 	// --- KONFIGURASI CORS ---
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://smile-fest.com", "https://smile-festival.ridwanafzn.workers.dev"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://smile-fest.com", "https://smile-festival.ridwanafzn.workers.dev", "https://smile-festival.pages.dev"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
